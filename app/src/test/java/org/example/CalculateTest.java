5d28316a35b85c16825fefdb2b3c831ba20c685a
shinnosuke <m1m24a29@oit.ac.jp>
2024-07-01 17:22:21 +0900

    add gradle project

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -8,6 +8,6 @@ public class CalculateTest {
   public void testSum() {
     Calculate calculate = new Calculate();
     int expected = 5;
-    assertEquals("Hello World!", calculate.sum(2, 3));
+    assertEquals(expected, calculate.sum(2, 3));
   }
 }
