703f3bee48d474139ab582e8942c550ce10317ac
shinnosuke <m1m24a29@oit.ac.jp>
2024-07-01 17:32:36 +0900

    add gradle project

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -4,4 +4,48 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public double average(int x, int y) {
+    return (x + y) / 2.0;
+  }
+
+  public int sumRange(int start, int end) {
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+
+  public double averageRange(int start, int end) {
+    return sumRange(start, end) / (double) (end - start + 1);
+  }
+
+  public int sumOddRange(int start, int end) {
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+      if (i % 2 != 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+
+  public int sumEvenRange(int start, int end) {
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+      if (i % 2 == 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    Calculate calc = new Calculate();
+    System.out.println("Sum of 2 and 3 is " + calc.sum(2, 3) + ". Average is " + calc.average(2, 3) + ".");
+    System.out.println("Sum of 1 to 10 is " + calc.sumRange(1, 10) + ". Average is " + calc.averageRange(1, 10) + ".");
+    System.out.println(
+        "Sum of odd of 1 to 10 is " + calc.sumOddRange(1, 10) + ". Sum of even is " + calc.sumEvenRange(1, 10) + ".");
+  }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -9,5 +9,7 @@ public class CalculateTest {
     Calculate calculate = new Calculate();
     int expected = 5;
     assertEquals(expected, calculate.sum(2, 3));
+    expected = 55;
+    assertEquals(expected, calculate.sum(1, 10));
   }
 }
