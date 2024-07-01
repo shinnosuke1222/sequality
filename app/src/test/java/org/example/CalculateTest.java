63ec8ecd41baabedcea1e01645434dc9b3aea882
shinnosuke <m1m24a29@oit.ac.jp>
2024-07-01 20:36:53 +0900

    add calculate-ver3

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -16,28 +16,28 @@ public class CalculateTest {
   public void testAverage() {
     Calculate calc = new Calculate();
     assertEquals(3, calc.average(2, 4));
-    assertEquals(2, calc.average(2, 4));
+    assertEquals(3, calc.average(2, 4));
   }
 
   @Test
   public void testSumRange() {
     Calculate calc = new Calculate();
     assertEquals(55, calc.sumRange(1, 10));
-    assertEquals(50, calc.sumRange(1, 10));
+    assertEquals(55, calc.sumRange(1, 10));
   }
 
   @Test
   public void testAverageRange() {
     Calculate calc = new Calculate();
     assertEquals(5, calc.averageRange(1, 10));
-    assertEquals(0, calc.averageRange(1, 10));
+    assertEquals(5, calc.averageRange(1, 10));
   }
 
   @Test
   public void testSumOddRange() {
     Calculate calc = new Calculate();
     assertEquals(25, calc.sumOddRange(1, 10));
-    assertEquals(2, calc.sumOddRange(1, 10));
+    assertEquals(25, calc.sumOddRange(1, 10));
   }
 
   @Test
