0b74ebb2776f7657dfdae2129c29dce6c96dd98d
shinnosuke <m1m24a29@oit.ac.jp>
2024-07-01 10:29:57 +0900

    add gradle project

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -15,6 +15,6 @@ public class AppTest {
   @Test
   public void testAppReternHelloWorld() {
     App classUnderTest = new App();
-    assertNotNull("Hello World!", classUnderTest.getGreeting());
+    assertEquals("Hello World", classUnderTest.getGreeting());
   }
 }
