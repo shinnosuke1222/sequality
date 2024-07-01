39cbb09c4c424768a8425314f5e9b47fff0548ae
shinnosuke <m1m24a29@oit.ac.jp>
2024-07-01 20:40:02 +0900

    add calculate-ver5

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -9,13 +9,11 @@ public class CalculateTest {
   public void testSum() {
     Calculate calc = new Calculate();
     assertEquals(5, calc.sum(2, 3));
-    assertEquals(1, calc.sum(2, -3));
   }
 
   @Test
   public void testAverage() {
     Calculate calc = new Calculate();
-    assertEquals(3, calc.average(2, 4));
     assertEquals(3, calc.average(2, 4));
   }
 
@@ -23,13 +21,11 @@ public class CalculateTest {
   public void testSumRange() {
     Calculate calc = new Calculate();
     assertEquals(55, calc.sumRange(1, 10));
-    assertEquals(55, calc.sumRange(1, 10));
   }
 
   @Test
   public void testAverageRange() {
     Calculate calc = new Calculate();
-    assertEquals(5, calc.averageRange(1, 10));
     assertEquals(5, calc.averageRange(1, 10));
   }
 
@@ -37,13 +33,11 @@ public class CalculateTest {
   public void testSumOddRange() {
     Calculate calc = new Calculate();
     assertEquals(25, calc.sumOddRange(1, 10));
-    assertEquals(25, calc.sumOddRange(1, 10));
   }
 
   @Test
   public void testSumEvenRange() {
     Calculate calc = new Calculate();
-    assertEquals(30, calc.sumEvenRange(1, 10));
     assertEquals(30, calc.sumEvenRange(1, 10));
   }
 }
